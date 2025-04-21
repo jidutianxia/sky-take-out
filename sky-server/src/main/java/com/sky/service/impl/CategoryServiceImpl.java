b29ae40148318a316feee9ccb135c9c052bfcdf0
jidutianxia <jdtx1244664354@163.com>
2025-04-21 08:44:01 +0800

    删除分类时进行关联判断

--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -2,12 +2,16 @@ package com.sky.service.impl;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.CategoryMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
 import com.sky.result.PageResult;
 import com.sky.service.CategoryService;
 import org.springframework.beans.BeanUtils;
@@ -22,6 +26,14 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
 
     @Override
     public void addCategory(CategoryDTO categoryDTO) {
@@ -52,6 +64,19 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void byIdDelete(String id) {
+        Integer count = dishMapper.countByCategoryId(Long.valueOf(id));
+        if(count > 0){
+            //当前分类下有菜品，不能删除
+            throw new DeletionNotAllowedException(MessageConstant.CATEGORY_BE_RELATED_BY_DISH);
+        }
+
+        //查询当前分类是否关联了套餐，如果关联了就抛出业务异常
+        count = setmealMapper.countByCategoryId(Long.valueOf(id));
+        if(count > 0){
+            //当前分类下有菜品，不能删除
+            throw new DeletionNotAllowedException(MessageConstant.CATEGORY_BE_RELATED_BY_SETMEAL);
+        }
+
         categoryMapper.byIdDelete();
     }
 
