12df74a5bc86a96d2738c34973d04fadc4f8aee4
jidutianxia <jdtx1244664354@163.com>
2025-04-30 13:01:51 +0800

    更新为Springboot 3.2.5版本，并且无法使用，仍存有bug

--- sky-server/src/main/java/com/sky/controller/admin/ChatController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ChatController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +21,13 @@ public class ChatController {
     @Autowired
     QwenChatModel qwenChatModel;
 
-    @Autowired
-    ChatLanguageModel deepseekModel;
+//    @Qualifier("openAiChatModel")
+//    @Autowired
+//    ChatLanguageModel OpenAiChatModel;
 
-    @Autowired
-    ChatLanguageModel ollamaChatModel;
+//    @Qualifier("ollamaChatModel")
+//    @Autowired
+//    ChatLanguageModel ollamaChatModel;
 
     @ApiOperation("千问")
     @RequestMapping(value = "/qwen",produces = "application/json;charset=UTF-8")
@@ -34,19 +37,19 @@ public class ChatController {
         return chat;
     }
 
-    @ApiOperation("deepseek")
-    @RequestMapping(value = "/deepseek", produces = "application/json;charset=UTF-8")
-    public String deepseekAI(@RequestParam(defaultValue = "你是谁") String message){
-        log.info("用户：{}",message);
-        String chat = deepseekModel.chat(message);
-        return chat;
-    }
-
-    @ApiOperation("ollama")
-    @RequestMapping(value = "/ollama", produces = "application/json;charset=UTF-8")
-    public String ollamaAI(@RequestParam(defaultValue = "你是谁") String message){
-        log.info("用户：{}",message);
-        String chat = ollamaChatModel.chat(message);
-        return chat;
-    }
+//    @ApiOperation("deepseek")
+//    @RequestMapping(value = "/deepseek", produces = "application/json;charset=UTF-8")
+//    public String deepseekAI(@RequestParam(defaultValue = "你是谁") String message){
+//        log.info("用户：{}",message);
+//        String chat = OpenAiChatModel.chat(message);
+//        return chat;
+//    }
+//
+//    @ApiOperation("ollama")
+//    @RequestMapping(value = "/ollama", produces = "application/json;charset=UTF-8")
+//    public String ollamaAI(@RequestParam(defaultValue = "你是谁") String message){
+//        log.info("用户：{}",message);
+//        String chat = ollamaChatModel.chat(message);
+//        return chat;
+//    }
 }
