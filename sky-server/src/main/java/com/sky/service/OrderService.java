1d8b300fab2b44cc3787902391c86dc40153396f
jidutianxia <jdtx1244664354@163.com>
2025-06-11 16:04:31 +0800

    客户催单 功能业务代码

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -30,4 +30,6 @@ public interface OrderService {
     void delivery(Long id);
 
     void complete(Long id);
+
+    void reminder(Long id);
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -378,4 +378,21 @@ public class OrderServiceImpl implements OrderService {
 
         orderMapper.update(orders);
     }
+
+    @Override
+    public void reminder(Long id) {
+        Orders ordersDB = orderMapper.getById(String.valueOf(id));
+
+        if(ordersDB == null){
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+
+        Map map = new HashMap();
+        map.put("type",2); // 1表示来单提醒 2表示客户催单
+        map.put("orderId",id);
+        map.put("content","订单号:"+ ordersDB.getNumber());
+
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
+    }
 }
