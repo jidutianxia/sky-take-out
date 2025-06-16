ce17627a4ef8e7069b7c458da1d7d149b5775c48
jidutianxia <jdtx1244664354@163.com>
2025-06-16 10:12:15 +0800

    导入工作台相关代码 实现功能接口

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -37,4 +38,6 @@ public interface DishMapper {
     List<Dish> getBySetmealId(Long id);
 
     List<Dish> list(Dish dish);
+
+    Integer countByMap(Map map);
 }
